d1eb46436efbdff3bb6d6e0fca08df15de128558
Wendall Cada <wendallc@83864.com>
2025-09-26 09:46:32 -0700

    Add block support for resource locations.

--- Common/src/main/java/technology/roughness/whitenoise/util/ResourceLocationHelper.java
+++ Common/src/main/java/technology/roughness/whitenoise/util/ResourceLocationHelper.java
@@ -3,6 +3,7 @@ package technology.roughness.whitenoise.util;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 
 public class ResourceLocationHelper {
 
@@ -22,8 +23,16 @@ public class ResourceLocationHelper {
         return BuiltInRegistries.ITEM.getKey(stack.getItem());
     }
 
+    public static ResourceLocation getBlockId(Block block) {
+        return BuiltInRegistries.BLOCK.getKey(block);
+    }
+
     public static String getModId(ItemStack stack) {
         return getItemStackId(stack).getNamespace();
+    }
+
+    public static String getModId(Block block) {
+        return getBlockId(block).getNamespace();
     }
 
 }
